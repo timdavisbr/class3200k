8bb485b247c64128b8234c55ee8130cd0abfa2fa
Timothy Davis <44853156+timdavisbr@users.noreply.github.com>
2019-03-25 23:02:36 -0400

    Fixed calc
    Made some changes to my tip calculator

--- world.py
+++ world.py
@@ -1,1 +1,11 @@
 print("Hello World!")
+
+# This is a tip calculator
+meal = 25.65
+tax = 0.07
+tip = 0.20
+total = meal + meal * tax
+tip_total = total * tip
+print ("tip: ${}".format(tip_total))
+meal_total = total + total * tip
+print ("total: ${}".format(meal_total))
